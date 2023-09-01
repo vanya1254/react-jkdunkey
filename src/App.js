221d4c5db8483a4863e0fa9c7fe4ce4c0ee81ce0
Ivan <vanya1254@yandex.ru>
2023-09-01 12:27:00 +0300

    some fixes and add folder for module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,69 @@ import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import Card from "./components/Card";
 
+const sneakers = [
+  {
+    id: 1,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    photo: "/img/sneakers/1.jpg",
+  },
+  {
+    id: 2,
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    photo: "/img/sneakers/2.jpg",
+  },
+  {
+    id: 3,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    photo: "/img/sneakers/3.jpg",
+  },
+  {
+    id: 4,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    photo: "/img/sneakers/4.jpg",
+  },
+  {
+    id: 5,
+    title: "Мужские Кроссовки Under Armour Curry 8",
+    price: 15199,
+    photo: "/img/sneakers/5.jpg",
+  },
+  {
+    id: 6,
+    title: "Мужские Кроссовки Nike Kyrie 7",
+    price: 11299,
+    photo: "/img/sneakers/6.jpg",
+  },
+  {
+    id: 7,
+    title: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 10799,
+    photo: "/img/sneakers/7.jpg",
+  },
+  {
+    id: 8,
+    title: "Мужские Кроссовки Nike LeBron XVIII",
+    price: 16499,
+    photo: "/img/sneakers/8.jpg",
+  },
+  {
+    id: 9,
+    title: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 13999,
+    photo: "/img/sneakers/9.jpg",
+  },
+  {
+    id: 10,
+    title: "Мужские Кроссовки Nike Kyrie Flytrap IV",
+    price: 11299,
+    photo: "/img/sneakers/10.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,9 +79,16 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
+          {sneakers.map((obj, key) => (
+            <Card
+              key={key}
+              id={obj.id}
+              title={obj.title}
+              price={obj.price}
+              photo={obj.photo}
+              onClickPlus={() => obj}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,9 @@
-function Card() {
+import styles from "./Card.module.scss";
+
+function Card(props) {
   return (
-    <div className="card mr-40">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img
           width={32}
           height={32}
@@ -9,14 +11,14 @@ function Card() {
           alt="Add to wishlist"
         />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.photo} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button onClick={props.onClickPlus} className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="Add to cart" />
         </button>
       </div>
