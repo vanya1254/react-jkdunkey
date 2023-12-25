5a19e8417e0872c62d5857b7dee018dafc3840ac
Ivan <vanya1254@yandex.ru>
2023-12-25 20:09:18 +0300

    fix scss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,7 +41,7 @@ const Home: React.FC<HomeProps> = ({
 
   return (
     <div className="content p-40">
-      <div className="d-flex align-center justify-between mb-40">
+      <div className="top d-flex align-center justify-between mb-40">
         <h1>
           {searchValue ? `Поиск по запросу: "${searchValue}"` : "Все кроссовки"}
         </h1>
